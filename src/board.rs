2af0d27321c6a5671b69c106f05a333d66b45160
Justin Browne <jebdude89@gmail.com>
2022-09-14 23:16:04 -0400

    Initial commit

---
+++ src/board.rs
@@ -0,0 +1,173 @@
+use egui::{Color32, ColorImage};
+use serde::{Deserialize, Serialize};
+use std::ops::{Index, IndexMut};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Board {
+    size: [usize; 2],
+    data: Vec<bool>,
+}
+
+impl Board {
+    #[must_use]
+    pub fn new(width: usize, height: usize) -> Self {
+        if let Some(length) = usize::checked_mul(width, height) {
+            let data = vec![false; length];
+            Self {
+                size: [width, height],
+                data,
+            }
+        } else {
+            panic!("board size of `({width}, {height})` is too large");
+        }
+    }
+
+    #[must_use]
+    pub fn size(&self) -> &[usize; 2] {
+        &self.size
+    }
+
+    #[must_use]
+    pub fn data(&self) -> &Vec<bool> {
+        &self.data
+    }
+
+    #[must_use]
+    pub fn get(&self, (y, x): (usize, usize)) -> Option<bool> {
+        let [width, height] = self.size;
+        (x < width && y < height).then(|| self.data[y * width + x])
+    }
+
+    #[must_use]
+    pub fn get_mut(&mut self, (y, x): (usize, usize)) -> Option<&mut bool> {
+        let [width, height] = self.size;
+        (x < width && y < height).then(|| &mut self.data[y * width + x])
+    }
+
+    fn live_neighbors(&self, (y, x): (usize, usize)) -> usize {
+        #[allow(clippy::range_minus_one)]
+        let x_iter = if x == 0 {
+            0..=1
+        } else if x == self.size[0] - 1 {
+            (self.size[0] - 2)..=(self.size[0] - 1)
+        } else {
+            (x - 1)..=(x + 1)
+        };
+        #[allow(clippy::range_minus_one)]
+        let y_iter = if y == 0 {
+            0..=1
+        } else if y == self.size[0] - 1 {
+            (self.size[1] - 2)..=(self.size[1] - 1)
+        } else {
+            (y - 1)..=(y + 1)
+        };
+
+        y_iter
+            .flat_map(|j| {
+                x_iter.clone().map(move |i| {
+                    if self[(j, i)] && !(i == x && j == y) {
+                        1
+                    } else {
+                        0
+                    }
+                })
+            })
+            .sum()
+    }
+
+    #[must_use]
+    pub fn next(&self) -> Self {
+        let mut next = Board::new(self.size[0], self.size[1]);
+        for j in 0..self.size[1] {
+            for i in 0..self.size[0] {
+                let live_neighbors = self.live_neighbors((j, i));
+                if live_neighbors == 3 || (live_neighbors == 2 && self[(j, i)]) {
+                    next[(j, i)] = true;
+                }
+            }
+        }
+        next
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = ((usize, usize), bool)> + '_ {
+        (0..self.size[1]).flat_map(move |j| (0..self.size[0]).map(move |i| ((j, i), self[(j, i)])))
+    }
+}
+
+impl Index<(usize, usize)> for Board {
+    type Output = bool;
+
+    fn index(&self, (y, x): (usize, usize)) -> &Self::Output {
+        // check that x and y are within the bounds of the board.
+        // since we check that `width * height` does not overflow in the constructor,
+        // we know that `y * width + x` does not overflow
+        let [width, height] = self.size;
+        assert!(x < width);
+        assert!(y < height);
+
+        &self.data[y * width + x]
+    }
+}
+
+impl IndexMut<(usize, usize)> for Board {
+    fn index_mut(&mut self, (y, x): (usize, usize)) -> &mut Self::Output {
+        // check that x and y are within the bounds of the board.
+        // since we check that `width * height` does not overflow in the constructor,
+        // we know that `y * width + x` does not overflow
+        let [width, height] = self.size;
+        assert!(x < width);
+        assert!(y < height);
+
+        &mut self.data[y * width + x]
+    }
+}
+
+impl From<&Board> for ColorImage {
+    fn from(board: &Board) -> ColorImage {
+        let Board { size, data } = board;
+        #[allow(clippy::clone_on_copy)]
+        let size = size.clone();
+        let pixels = data
+            .iter()
+            .map(|b| if *b { Color32::BLACK } else { Color32::WHITE })
+            .collect();
+        ColorImage { size, pixels }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Board;
+
+    #[test]
+    fn new_board() {
+        let _board = Board::new(2, 3);
+        let _board = Board::new(1000, 1000);
+    }
+
+    #[test]
+    #[should_panic]
+    fn new_board_panic() {
+        let _board = Board::new(usize::MAX, 2);
+    }
+
+    #[test]
+    fn live_neighbors() {
+        let mut board = Board::new(3, 3);
+        board[(0, 1)] = true;
+        board[(1, 2)] = true;
+        board[(2, 0)] = true;
+        board[(2, 1)] = true;
+        board[(2, 2)] = true;
+
+        assert_eq!(board.live_neighbors((0, 0)), 1);
+        assert_eq!(board.live_neighbors((0, 1)), 1);
+        assert_eq!(board.live_neighbors((0, 2)), 2);
+        assert_eq!(board.live_neighbors((1, 0)), 3);
+        assert_eq!(board.live_neighbors((1, 1)), 5);
+        assert_eq!(board.live_neighbors((1, 2)), 3);
+        assert_eq!(board.live_neighbors((2, 0)), 1);
+        assert_eq!(board.live_neighbors((2, 1)), 3);
+        assert_eq!(board.live_neighbors((2, 2)), 2);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,191 @@
+#![feature(io_read_to_string)]
+
+use egui::{Color32, Pos2, Rect, Rounding, Sense, Vec2};
+use egui_miniquad as egui_mq;
+use gol::Board;
+use miniquad as mq;
+use serde_json::{from_reader, to_writer_pretty};
+use std::{
+    fs::File,
+    io::{BufReader, BufWriter},
+};
+
+const BOARD_SCALE: f32 = 10.0;
+
+struct Stage {
+    egui_mq: egui_mq::EguiMq,
+    filename: String,
+    edit_mode: bool,
+    board: Option<Board>,
+}
+
+impl Stage {
+    fn new(ctx: &mut mq::Context) -> Self {
+        let board = Some(Board::new(30, 30));
+        Self {
+            egui_mq: egui_mq::EguiMq::new(ctx),
+            filename: String::new(),
+            edit_mode: false,
+            board,
+        }
+    }
+}
+
+impl mq::EventHandler for Stage {
+    fn update(&mut self, _ctx: &mut mq::Context) {}
+
+    fn draw(&mut self, mq_ctx: &mut mq::Context) {
+        mq_ctx.clear(Some((1., 1., 1., 1.)), None, None);
+        mq_ctx.begin_default_pass(mq::PassAction::clear_color(0.2, 0.2, 0.2, 1.0));
+        mq_ctx.end_render_pass();
+
+        self.egui_mq.run(mq_ctx, |_mq_ctx, egui_ctx| {
+            egui::TopBottomPanel::top("top").show(egui_ctx, |ui| {
+                ui.horizontal(|ui| {
+                    let filename = &mut self.filename;
+                    ui.label("Filename:");
+                    ui.text_edit_singleline(filename);
+                    if ui.button("Save").clicked() {
+                        if let Ok(file) = File::create(&filename) {
+                            let file = BufWriter::new(file);
+                            if to_writer_pretty(file, &self.board).is_err() {
+                                eprintln!("error writing board to {filename}");
+                            }
+                        } else {
+                            eprintln!("could not write to {filename}");
+                        }
+                    }
+                    if ui.button("Load").clicked() {
+                        if let Ok(file) = File::open(&filename) {
+                            let file = BufReader::new(file);
+                            if let Ok(board) = from_reader(file) {
+                                self.board = Some(board);
+                            } else {
+                                eprintln!("invalid board data in {filename}");
+                            }
+                        } else {
+                            eprintln!("could not read from {filename}");
+                        }
+                    }
+                });
+            });
+
+            egui::TopBottomPanel::bottom("bottom").show(egui_ctx, |ui| {
+                ui.horizontal(|ui| {
+                    ui.toggle_value(&mut self.edit_mode, "Edit");
+                    if ui.button("Next").clicked() {
+                        if let Some(board) = self.board.as_ref() {
+                            self.board = Some(board.next());
+                        }
+                    }
+                    #[cfg(not(target_arch = "wasm32"))]
+                    {
+                        if ui.button("Quit").clicked() {
+                            std::process::exit(0);
+                        }
+                    }
+                });
+            });
+
+            egui::CentralPanel::default().show(egui_ctx, |ui| {
+                egui::ScrollArea::both().show(ui, |ui| {
+                    if let Some(board) = self.board.as_mut() {
+                        let (response, painter) =
+                            ui.allocate_painter(Vec2::splat(300.0), Sense::click());
+                        painter.rect_filled(response.rect, Rounding::none(), Color32::WHITE);
+                        let min = response.rect.min;
+                        for ((j, i), _) in board.iter().filter(|(_, live)| *live) {
+                            #[allow(clippy::cast_precision_loss)]
+                            let pix_min = Pos2 {
+                                x: min.x + BOARD_SCALE * i as f32,
+                                y: min.y + BOARD_SCALE * j as f32,
+                            };
+                            #[allow(clippy::cast_precision_loss)]
+                            let pix_max = Pos2 {
+                                x: min.x + BOARD_SCALE * (i + 1) as f32,
+                                y: min.y + BOARD_SCALE * (j + 1) as f32,
+                            };
+                            painter.rect_filled(
+                                Rect::from_two_pos(pix_min, pix_max),
+                                Rounding::none(),
+                                Color32::BLACK,
+                            );
+                        }
+                        #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+                        if self.edit_mode && response.clicked() &&
+                            let Some(pos) = response.interact_pointer_pos() {
+                                let pix = ((pos - min) / BOARD_SCALE).floor();
+                                let x = pix[0] as usize;
+                                let y = pix[1] as usize;
+                                board[(y, x)] = !board[(y, x)];
+                        }
+                    }
+                });
+            });
+        });
+
+        self.egui_mq.draw(mq_ctx);
+        mq_ctx.commit_frame();
+    }
+
+    fn mouse_motion_event(&mut self, _: &mut mq::Context, x: f32, y: f32) {
+        self.egui_mq.mouse_motion_event(x, y);
+    }
+
+    fn mouse_wheel_event(&mut self, _: &mut mq::Context, dx: f32, dy: f32) {
+        self.egui_mq.mouse_wheel_event(dx, dy);
+    }
+
+    fn mouse_button_down_event(
+        &mut self,
+        ctx: &mut mq::Context,
+        mb: mq::MouseButton,
+        x: f32,
+        y: f32,
+    ) {
+        self.egui_mq.mouse_button_down_event(ctx, mb, x, y);
+    }
+
+    fn mouse_button_up_event(
+        &mut self,
+        ctx: &mut mq::Context,
+        mb: mq::MouseButton,
+        x: f32,
+        y: f32,
+    ) {
+        self.egui_mq.mouse_button_up_event(ctx, mb, x, y);
+    }
+
+    fn char_event(
+        &mut self,
+        _ctx: &mut mq::Context,
+        character: char,
+        _keymods: mq::KeyMods,
+        _repeat: bool,
+    ) {
+        self.egui_mq.char_event(character);
+    }
+
+    fn key_down_event(
+        &mut self,
+        ctx: &mut mq::Context,
+        keycode: mq::KeyCode,
+        keymods: mq::KeyMods,
+        _repeat: bool,
+    ) {
+        self.egui_mq.key_down_event(ctx, keycode, keymods);
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut mq::Context, keycode: mq::KeyCode, keymods: mq::KeyMods) {
+        self.egui_mq.key_up_event(keycode, keymods);
+    }
+}
+
+fn main() {
+    let conf = mq::conf::Conf {
+        window_title: "Conway's Game of Life".to_string(),
+        high_dpi: true,
+        ..Default::default()
+    };
+    mq::start(conf, |ctx| Box::new(Stage::new(ctx)));
+}
